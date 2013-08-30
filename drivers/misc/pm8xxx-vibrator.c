af90c34f4043271c59726a66911ce628721b5049
Darescu Ionut <ionut.darescu@gmail.com>
2013-08-30 22:31:01 +0300

    pm8xxx-vibrator: exposed sysfs to modify voltage level
    sysfs path: sys/devices/platform/msm_ssbi.0/pm8058-core/pm8xxx-vib/amp
    
    credit: ngvincent

--- drivers/misc/pm8xxx-vibrator.c
+++ drivers/misc/pm8xxx-vibrator.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2011, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -20,6 +20,8 @@
 #include <linux/mfd/pm8xxx/core.h>
 #include <linux/mfd/pm8xxx/vibrator.h>
 
+#include "../staging/iio/sysfs.h"
+
 #include "../staging/android/timed_output.h"
 
 #define VIB_DRV			0x4A
@@ -31,6 +33,8 @@
 
 #define VIB_MAX_LEVEL_mV	3100
 #define VIB_MIN_LEVEL_mV	1200
+
+static unsigned int vib_level_range=(VIB_MAX_LEVEL_mV - VIB_MIN_LEVEL_mV) / 100;
 
 struct pm8xxx_vib {
 	struct hrtimer vib_timer;
@@ -198,6 +202,36 @@ static enum hrtimer_restart pm8xxx_vib_timer_func(struct hrtimer *timer)
 	return HRTIMER_NORESTART;
 }
 
+static ssize_t pm8xxx_vib_voltage_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	int level = vib_dev->level ;
+
+	// convert to percentage form
+	level = (level - VIB_MIN_LEVEL_mV/100) * 100 / vib_level_range ;
+
+	return sprintf(buf, "%d\n", level);
+}
+
+static ssize_t pm8xxx_vib_voltage_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+        int level_multiply;
+	sscanf(buf, "%d", &level_multiply);
+	if (level_multiply > 100)
+		level_multiply = 100;
+	else if (level_multiply < 0)
+		level_multiply = 0;
+
+	vib_dev->level = VIB_MIN_LEVEL_mV/100 + vib_level_range * level_multiply / 100;
+
+	return size;
+}
+
+static struct device_attribute pm8xxx_vib_devie_attrs[] = {
+        __ATTR(amp, S_IWUSR | S_IRUGO, pm8xxx_vib_voltage_show, pm8xxx_vib_voltage_store),
+};
+
 #ifdef CONFIG_PM
 static int pm8xxx_vib_suspend(struct device *dev)
 {
@@ -216,41 +250,6 @@ static const struct dev_pm_ops pm8xxx_vib_pm_ops = {
 };
 #endif
 
-static ssize_t voltage_level_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct timed_output_dev *time_cdev;
-	struct pm8xxx_vib *vib;
-
-	time_cdev = (struct timed_output_dev *) dev_get_drvdata(dev);
-	vib = container_of(time_cdev, struct pm8xxx_vib, timed_dev);
-	return sprintf(buf, "%d\n", vib->level * 100);
-}
-
-static ssize_t voltage_level_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t size)
-{
-	struct timed_output_dev *time_cdev;
-	struct pm8xxx_vib *vib;
-	int voltage_input = -1;
-
-	time_cdev = (struct timed_output_dev *) dev_get_drvdata(dev);
-	vib = container_of(time_cdev, struct pm8xxx_vib, timed_dev);
-
-	sscanf(buf, "%d ", &voltage_input);
-	if (voltage_input < VIB_MIN_LEVEL_mV ||
-			voltage_input > VIB_MAX_LEVEL_mV) {
-		pr_err("%s: invalid voltage level input: %d",
-				__func__, voltage_input);
-		return -EINVAL;
-	}
-
-	vib->level = voltage_input / 100;
-	return size;
-}
-static DEVICE_ATTR(voltage_level, S_IRUGO | S_IWUSR, voltage_level_show,
-		voltage_level_store);
-
 static int __devinit pm8xxx_vib_probe(struct platform_device *pdev)
 
 {
@@ -259,7 +258,7 @@ static int __devinit pm8xxx_vib_probe(struct platform_device *pdev)
 	struct pm8xxx_vib *vib;
 	u8 val;
 	int rc;
-
+	int i;
 	if (!pdata)
 		return -EINVAL;
 
@@ -305,21 +304,31 @@ static int __devinit pm8xxx_vib_probe(struct platform_device *pdev)
 	if (rc < 0)
 		goto err_read_vib;
 
-	rc = device_create_file(vib->timed_dev.dev, &dev_attr_voltage_level);
-	if (rc < 0) {
-		pr_err("%s: failed to add voltage_level sysfs node", __func__);
-	}
-
 	pm8xxx_vib_enable(&vib->timed_dev, pdata->initial_vibrate_ms);
 
 	platform_set_drvdata(pdev, vib);
 
 	vib_dev = vib;
+	for (i = 0; i < ARRAY_SIZE(pm8xxx_vib_devie_attrs); i++) {
+		rc = device_create_file(vib->dev,
+				&pm8xxx_vib_devie_attrs[i]);
+		if (rc < 0) {
+			pr_err("%s: failed to create sysfs\n", __func__);
+			goto err_sysfs;
+		}
+	}
 
 	return 0;
 
 err_read_vib:
 	kfree(vib);
+	return rc;
+
+err_sysfs:
+	for (; i >= 0; i--) {
+		device_remove_file(vib->dev,
+				&pm8xxx_vib_devie_attrs[i]);
+	}
 	return rc;
 }
 
